63859f43c8474553b2372e3cbfa08e1063455430
erikfors <efors003@fiu.edu>
2021-09-17 19:35:47 -0400

    Performance fix

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -33,7 +33,7 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Pedro"),
+        title: Text("Main Room"),
         actions: [
           DropdownButton(
             underline: Container(),
